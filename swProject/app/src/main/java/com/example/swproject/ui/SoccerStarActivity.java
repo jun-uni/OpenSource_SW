2e5eb7d72c27574e398f0cf3d37bd5bb0ab2908e
CH <chanhue467@gmail.com>
2022-05-30 14:54:17 +0900

    addalarm

--- swProject/app/src/main/java/com/example/swproject/ui/SoccerStarActivity.java
+++ swProject/app/src/main/java/com/example/swproject/ui/SoccerStarActivity.java
@@ -1,17 +1,26 @@
 package com.example.swproject.ui;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
+import com.example.swproject.Alarm.AlarmRecevier;
 import com.example.swproject.R;
 import com.example.swproject.data.SoccerData;
 import com.example.swproject.util.MyParser;
@@ -21,8 +30,23 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class SoccerStarActivity extends soccerActivity{
+    /* 알람 기능 추가 변수*/
+    private AlarmManager alarmManager;
+    private GregorianCalendar mCalender;
+
+    private NotificationManager notificationManager;
+    NotificationCompat.Builder builder;
+    /* 알람 기능 추가 변수*/
+
+
+
     private TextView live_data_;
     private boolean is_playing_ = false;
 
@@ -40,7 +64,59 @@ public class SoccerStarActivity extends soccerActivity{
 
         Button btnRefresh = findViewById(R.id.btnRefresh);
         btnRefresh.setOnClickListener(view -> new GetLive().execute(schedule_url_));
+
+        /* 알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가*/
+
+        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+
+        mCalender = new GregorianCalendar();
+
+        Log.v("HelloAlarmActivity", mCalender.getTime().toString());
+
+        setContentView(R.layout.soccer_star);
+
+        Button button = (Button) findViewById(R.id.btnNoti);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarm();
+            }
+        });
+        /* 알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가*/
+
+
+
+
     }
+    /* 알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가*/
+
+    private void setAlarm() {
+        //AlarmReceiver에 값 전달
+        Intent receiverIntent = new Intent(SoccerStarActivity.this, AlarmRecevier.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(SoccerStarActivity.this, 0, receiverIntent, 0);
+
+        String from = "2022-05-30 05:50:15"; //임의로 날짜와 시간을 지정
+
+        //날짜 포맷을 바꿔주는 소스코드
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date datetime = null;
+        try {
+            datetime = dateFormat.parse(from);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(datetime);
+
+        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(),pendingIntent);
+
+
+    }
+    /* 알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가알람 기능 추가*/
+
 
     @SuppressLint("StaticFieldLeak")
     private class GetLive extends AsyncTask<String, Void, Void> {
@@ -116,6 +192,7 @@ public class SoccerStarActivity extends soccerActivity{
             str += "총 패스 [" + data.GetTeamLeft().GetTotalPass() + " : " + data.GetTeamRight().GetTotalPass() + "]";
 
             live_data_.setText(str);
+
         }
     }
 }
