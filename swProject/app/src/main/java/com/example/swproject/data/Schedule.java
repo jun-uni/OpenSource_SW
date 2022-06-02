e6c8ba34416f936bb6025a9ef308bb73405a6a9c
hayeon <gimhayeon@gmail.com>
2022-06-03 01:09:50 +0900

    머지 충돌 수정

--- swProject/app/src/main/java/com/example/swproject/data/Schedule.java
+++ swProject/app/src/main/java/com/example/swproject/data/Schedule.java
@@ -11,21 +11,25 @@ public class Schedule {
     private ZonedDateTime date_;             //경기 날짜, 시간
     private Team left_;
     private Team right_;
-    private boolean isPlaying_;
-
+    private boolean is_playing_;
+    private String stadium_;                 //경기장
+    private String broadcast_;               //방송사
+    private boolean is_canceled_;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public Schedule(){
-       date_ = ZonedDateTime.now();
-       left_ = new Team();
-       right_ = new Team();
-       isPlaying_ = false;
+        date_ = ZonedDateTime.now();
+        left_ = new Team();
+        right_ = new Team();
+        is_playing_ = false;
+        stadium_ = "";
+        broadcast_ = "";
+        is_canceled_ = false;
     }
 
     public static class Team{
         private String name_;       //팀 이름
         private int score_;         //점수
-
         Team(){
             name_ = "";
             score_ = 0;
@@ -50,13 +54,21 @@ public class Schedule {
         return this.date_.isBefore(ZonedDateTime.now());
     }
 
-    public void SetDate(ZonedDateTime date){ this.date_ = date;}
+    public void SetDate(ZonedDateTime date){
+        this.date_ = date;
+    }
 
     public void SetTeamLeft(Team team) { this.left_ = team; }
 
     public void SetTeamRight(Team team) { this.right_ = team; }
 
-    public void SetIsPlaying(boolean isPlaying) { this.isPlaying_ = isPlaying; }
+    public void SetIsPlaying(boolean is_playing) { this.is_playing_ = is_playing; }
+
+    public void SetIsCanceled(boolean is_canceled_) { this.is_canceled_ = is_canceled_; }
+
+    public void SetStadium(String stadium) { this.stadium_ = stadium; }
+
+    public void SetBroadcast(String broadcast) { this.broadcast_ = broadcast; }
 
     public Team GetTeamLeft() { return this.left_; }
 
@@ -64,5 +76,11 @@ public class Schedule {
 
     public ZonedDateTime GetDate() { return this.date_; }
 
-    public boolean GetIsPlaying() { return this.isPlaying_; }
+    public boolean GetIsPlaying() { return this.is_playing_; }
+
+    public boolean GetIsCanceled() { return this.is_canceled_; }
+
+    public String GetStadium() { return this.stadium_; }
+
+    public String GetBroadcast() { return this.broadcast_; }
 }
