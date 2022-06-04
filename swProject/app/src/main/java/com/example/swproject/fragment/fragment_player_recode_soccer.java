6869a74d280a5d21eeaa5adf8cbeaf984539ef27
hayeon <gimhayeon@gmail.com>
2022-06-04 22:33:44 +0900

    축구 선수 기록화면에 뉴스 뜨는거 오류 수정

--- swProject/app/src/main/java/com/example/swproject/fragment/fragment_player_recode_soccer.java
+++ swProject/app/src/main/java/com/example/swproject/fragment/fragment_player_recode_soccer.java
@@ -16,6 +16,6 @@ public class fragment_player_recode_soccer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player_recode_soccer2, container, false);
+        return inflater.inflate(R.layout.fragment_player_recode_soccer, container, false);
     }
 }
